2e6ae8f766be6d71ff1e26506d2c8dd32281ab9a
docoder <docoder@163.com>
2019-01-10 11:56:54 +0800

    Overall loading changed

--- src/components/UI/Loading/index.js
+++ src/components/UI/Loading/index.js
@@ -7,13 +7,15 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import {
     Spin,
-    message
+    message,
+    Icon
 } from 'antd';
+
 const SpinContainer = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    background-color: rgba(255,255,255,0.2);
+    background-color: rgba(255,255,255,0.01);
     position: fixed;
     left: 0;
     right: 0;
@@ -21,6 +23,17 @@ const SpinContainer = styled.div`
     bottom: 0;
     z-index: 1000;
 `;
+const SpinBox = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: rgba(0,32,64,0.6);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 10px;
+`;
+
+const spinIcon = <Icon type="loading-3-quarters" style={{ fontSize: 32, color: 'white' }} spin />;
 export default class Loading extends React.Component {
     componentDidMount() {
         this.timer = setTimeout(()=>{
@@ -44,7 +57,7 @@ export default class Loading extends React.Component {
             return null;
             case 'overall':
             return(
-                this.props.show ? <SpinContainer><Spin /></SpinContainer> : null
+                this.props.show ? <SpinContainer><SpinBox><Spin size="large" indicator={spinIcon} /></SpinBox></SpinContainer> : null
             )
         }
         
