0b5a85f5a15c7a1c0dce66bc82872a58f9758eb3
docoder <docoder@163.com>
2019-01-10 12:10:54 +0800

    Overall loading message

--- src/components/UI/Loading/index.js
+++ src/components/UI/Loading/index.js
@@ -24,13 +24,18 @@ const SpinContainer = styled.div`
     z-index: 1000;
 `;
 const SpinBox = styled.div`
-    width: 100px;
-    height: 100px;
+    padding: 30px 30px;
     background-color: rgba(0,32,64,0.6);
     display: flex;
     justify-content: center;
     align-items: center;
     border-radius: 10px;
+`;
+const StyledSpin = styled(Spin)`
+    &.ant-spin.ant-spin-show-text .ant-spin-text {
+        color: white;
+        padding-top: 10px;
+    }
 `;
 
 const spinIcon = <Icon type="loading-3-quarters" style={{ fontSize: 32, color: 'white' }} spin />;
@@ -57,7 +62,7 @@ export default class Loading extends React.Component {
             return null;
             case 'overall':
             return(
-                this.props.show ? <SpinContainer><SpinBox><Spin size="large" indicator={spinIcon} /></SpinBox></SpinContainer> : null
+                this.props.show ? <SpinContainer><SpinBox><StyledSpin tip={this.props.message} size="large" indicator={spinIcon} /></SpinBox></SpinContainer> : null
             )
         }
         
